bdb6720639909daec96d32f47b22a13a95e92257
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:06:04 +0530

    improve

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @like = likes(:one) # assuming you have a fixture set up for Like model
+  end
+
+  test "like should belong to a user" do
+    assert @like.user
+  end
+
+  test "like should belong to a post" do
+    assert @like.post
+  end
 end
