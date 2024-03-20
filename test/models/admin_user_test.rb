7c9eebcccd962df111e9752c6df820a9acb3cc0e
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:06:00 +0530

    improve

--- test/models/admin_user_test.rb
+++ test/models/admin_user_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class AdminUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "creation of an admin user" do
+    admin = AdminUser.new(email: "admin@example.com", password: "password", password_confirmation: "password", is_admin: true)
+    assert admin.save
+  end
 end
