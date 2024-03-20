0b86b903a631ecd432a683e771897be1c634e354
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:05:52 +0530

    improve

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,16 +1,15 @@
 class FollowsController < ApplicationController
-    before_action :authenticate_user!
-  
-    def create
-      @user = User.find(params[:followed_id])
-      current_user.follow(@user)
-      redirect_to @user
-    end
-  
-    def destroy
-      @user = Follow.find(params[:id]).followed
-      current_user.unfollow(@user)
-      redirect_to @user
-    end
+  before_action :authenticate_user!
+
+  def create
+    @user = User.find(params.permit(:followed_id).values.first)
+    current_user.follow(@user)
+    redirect_to @user
   end
-  
+
+  def destroy
+    @user = Follow.find(params[:id]).followed
+    current_user.unfollow(@user)
+    redirect_to @user
+  end
+end
