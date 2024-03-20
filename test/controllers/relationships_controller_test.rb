65406bcd8cfb5dd52355a01328bf3b77bd1075ab
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:06:10 +0530

    improve

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class RelationshipsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:michael)
+    @other_user = users(:archer)
+    sign_in @user
+  end
+
+  test "should follow a user" do
+    assert_difference "@user.following.count", 1 do
+      post relationships_path, params: { followed_id: @other_user.id }
+    end
+    assert_redirected_to @other_user
+  end
+
+  test "should unfollow a user" do
+    @user.follow(@other_user)
+    relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+    assert_difference "@user.following.count", -1 do
+      delete relationship_path(relationship)
+    end
+    assert_redirected_to @other_user
+  end
 end
+
