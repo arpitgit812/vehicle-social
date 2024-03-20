fd31dc9b5178142e6f823f54c3515f4c22956d13
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:05:57 +0530

    improve

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,18 @@
 class ProfilesController < ApplicationController
   def show
-      @user = User.find_by(username: params[:username])
+    @user = User.find_by(username: params[:username])
+
+    if @user.nil?
+      render_not_found
+    end
+  end
+
+  private
+
+  def render_not_found
+    render file: Rails.root.join('public', '404.html'), status: :not_found
   end
 end
+
+
+@user = User.find_by(username: params.fetch(:username) { raise ActionController::ParameterMissing, 'username is missing' })
