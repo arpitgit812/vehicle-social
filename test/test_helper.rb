b9b986e0c43be60e0833075234a7e1e47b0c4b49
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:06:09 +0530

    improve

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,21 @@ require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  parallelize(workers: :number_of_processors, with: :threads)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  # Helper method to cleanup database transactions after each test
+  def teardown
+    DatabaseCleaner.clean
+  end
+
+  # Helper method to setup database transactions before each test
+  def setup
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
+  end
 end
