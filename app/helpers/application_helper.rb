890e90e9b2c7d35ca90347663a451471f318aa6b
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:05:55 +0530

    improve

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+ 
+  # This module contains helper methods that can be used throughout the application.
+  # For example, you can add view logic here to be reused in different views.
+  #
+  # Example:
+  #
+  # def full_title(page_title)
+  #   base_title = "Your App Name"
+  #   page_title.empty? ? base_title : "#{base_title} | #{page_title}"
+  # end
+ 
 end
+
