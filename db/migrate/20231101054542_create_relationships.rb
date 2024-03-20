20fbbdc2302211b9efd6f82ca9f72dbb39ae4401
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:05:58 +0530

    improve

--- db/migrate/20231101054542_create_relationships.rb
+++ db/migrate/20231101054542_create_relationships.rb
@@ -1,8 +1,10 @@
 class CreateRelationships < ActiveRecord::Migration[7.0]
   def change
-    create_table :relationships do |t|
-
+    create_table :relationships, id: false do |t|
+      t.belongs_to :follower, index: true, foreign_key: { to_table: :users }
+      t.belongs_to :followed, index: true, foreign_key: { to_table: :users }
       t.timestamps
     end
   end
 end
+
