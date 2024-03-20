d7a43f6868e74157ee849ef6b6a5ce6dae938045
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:06:01 +0530

    improve

--- app/helpers/relationships_helper.rb
+++ app/helpers/relationships_helper.rb
@@ -1,2 +1,13 @@
 module RelationshipsHelper
+  def following?(current_user, user)
+    current_user.active_relationships.find_by(followed_id: user.id)
+  end
+
+  def follow_button(current_user, user)
+    if following?(current_user, user)
+      button_to 'Unfollow', relationship_path(current_user.active_relationships.find_by(followed_id: user.id)), method: :delete, class: 'btn btn-danger'
+    else
+      button_to 'Follow', relationships_path(followed_id: user.id), class: 'btn btn-primary'
+    end
+  end
 end
