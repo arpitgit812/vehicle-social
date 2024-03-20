d7019d6dea64f9c5230ad555ab868023561464dc
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:06:01 +0530

    improve

--- db/migrate/20231226105756_create_comments.rb
+++ db/migrate/20231226105756_create_comments.rb
@@ -1,11 +1,16 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.text :content
-      t.references :user, null: false, foreign_key: true
-      t.references :commentable, polymorphic: true, null: false
-
+      t.text :content, null: false
+      t.integer :user_id, null: false
+      t.integer :commentable_id, null: false
+      t.string :commentable_type, null: false
       t.timestamps
     end
+
+    add_index :comments, :user_id
+    add_index :comments, [:commentable_type, :commentable_id]
+    add_foreign_key :comments, :users, column: :user_id
+    add_foreign_key :comments, :commentables, column: :commentable_id
   end
 end
