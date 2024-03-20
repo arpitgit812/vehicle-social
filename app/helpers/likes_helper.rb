b3710c9bb27445c140eab84053a04b60cb449e39
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:05:57 +0530

    improve

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,17 @@
 module LikesHelper
+  # Returns the HTML for a link to like a given object
+  def like_link(object)
+    like = Like.find_by(user: current_user, likeable: object)
+    if like
+      link_to "Unlike", like_path(like), method: :delete
+    else
+      link_to "Like", like_path(object), method: :post
+    end
+  end
+
+  # Returns the number of likes for a given object
+  def like_count(object)
+    object.likes.size
+  end
 end
+
