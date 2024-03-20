562ab4ee4d81c033b0d29c3fdfe4b1339f53e233
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:05:54 +0530

    improve

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  include ActionController::Permitter
 
-    protected
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    end
+  protected
+
+  def configure_permitted_parameters
+    permittable_params(:sign_up, keys: [:username])
+  end
 end
