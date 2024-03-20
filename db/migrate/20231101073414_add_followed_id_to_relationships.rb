063085f7b05128ebaa71f3aa0cdac0f937834563
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:05:59 +0530

    improve

--- db/migrate/20231101073414_add_followed_id_to_relationships.rb
+++ db/migrate/20231101073414_add_followed_id_to_relationships.rb
@@ -1,5 +1,5 @@
 class AddFollowedIdToRelationships < ActiveRecord::Migration[7.0]
   def change
-    add_reference :relationships, :followed, foreign_key: { to_table: :users }
+    add_reference :relationships, :followed, foreign_key: { to_table: :users }, index: true
   end
 end
