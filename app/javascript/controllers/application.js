1e4e76ec95dc5e4d0d6000b84e2f1ab2a0cc7f46
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:05:55 +0530

    improve

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,13 @@
 import { Application } from "@hotwired/stimulus"
 
+// Create a new Stimulus application instance
 const application = Application.start()
 
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+// Enable or disable debug mode
+application.debug = process.env.NODE_ENV !== 'production'
 
+// Assign the application instance to the global window object
+window.Stimulus = application
+
+// Export the application instance for testing or other purposes
 export { application }
