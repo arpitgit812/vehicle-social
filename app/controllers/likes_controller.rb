0a466e54c1d2b31abaaa970ce035f7dafce15143
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:05:52 +0530

    improve

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,33 +1,9 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
 
-  def create_likes
-    @post = Post.find(params[:id])
-    @like = Like.new(user: current_user, likeable: @post)
+  def create
+    @likeable = find_likeable
+    @like = current_user.likes.new(likeable: @likeable)
 
     if @like.save
-      flash[:notice] = 'Post liked!'
-    else
-      flash[:alert] = 'Failed to like the post but un.'
-    end
 
-    redirect_back(fallback_location: root_path)
-  end
-
-  def destroy
-    @post = Post.find(params[:post_id])
-    @like = @post.likes.find(params[:format])
-
-    if @like.destroy
-      flash[:notice] = 'Post unliked!'
-    else
-      flash[:alert] = 'Failed to unlike the post.'
-    end
-
-    redirect_back(fallback_location: root_path)
-  end
-
-  def show
-    # Add logic for displaying likes if needed
-  end
-end
