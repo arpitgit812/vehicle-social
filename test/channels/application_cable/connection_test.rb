8495dbdc3a5c65b08cff2e2935b63340d585cdb8
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:05:58 +0530

    improve

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,11 +1,17 @@
 require "test_helper"
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
-  # test "connects with cookies" do
-  #   cookies.signed[:user_id] = 42
-  #
-  #   connect
-  #
-  #   assert_equal connection.user_id, "42"
-  # end
+  test "connects with user id in cookies" do
+    cookies.signed[:user_id] = 42
+
+    connect
+
+    assert_equal connection.user_id.to_s, "42"
+  end
+
+  test "disconnects when user id is not present in cookies" do
+    connect
+
+    refute connection.try(:user_id)
+  end
 end
