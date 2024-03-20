6d0284b36d59e43d48491f3b35f7cfdcdaf41337
arpitgit812 <125976466+arpitgit812@users.noreply.github.com>
2024-03-20 17:05:51 +0530

    improve

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @latest_posts = Post.latest(10)
   end
 end
+
+# in the Post model
+class Post < ApplicationRecord
+  def self.latest(n)
+    order(created_at: :desc).limit(n)
+  end
+end
+
